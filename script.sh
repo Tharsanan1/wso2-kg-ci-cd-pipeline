afb2c0413091d66a9fc5ab4fd2ad2af5402fd987
Tharsanan1 <ktharsanan@gmail.com>
2025-03-26 12:43:14 +0530

    Example curl

--- script.sh
+++ script.sh
@@ -5,3 +5,8 @@ git checkout dev
 cd ..
 cd dev/greetingAPI/1.0.0
 kustomize build .  
+
+
+curl -k --location 'https://dev.gw.example.com:9095/employees-info/3.14/employees' \
+--header 'Host: dev.gw.example.com' \
+--header 'Authorization: Bearer <>'
